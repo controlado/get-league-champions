0d9e05dcc27e9a347e0d19c34b4016d939a5feef
Yan Gabriel <yangabrielogy@gmail.com>
2022-08-02 13:11:42 -0300

    Update: skins adicionadas ao retorno

--- source/main.py
+++ source/main.py
@@ -6,28 +6,48 @@ import json
 
 class Riot:
 
-    versions_url = "https://ddragon.leagueoflegends.com/api/versions.json"
-
     def __init__(self):
         self.latest_version = self.get_versions()[0]
         self.champions_data = self.get_champions_data()
+        self.skins_data = self.get_skins_data()
 
-    def get_champions_names(self) -> list:
-        return [self.champions_data[champion]["name"] for champion in self.champions_data]
+    def get_database(self) -> list:
+        return [
+            {
+                "championId": self.champions_data[champion]["key"],
+                "championName": self.champions_data[champion]["name"],
+                "championSkins": self.get_champion_skins(
+                    self.champions_data[champion]["key"]
+                )
+            }
+            for champion in self.champions_data
+        ]
+
+    def get_champion_skins(self, requested_champion_id: str) -> list:
+        return [
+            {
+                "skinId": self.skins_data[skin]["id"],
+                "skinName": self.skins_data[skin]["name"],
+                "skinRarity": self.skins_data[skin]["rarity"]
+            }
+            for skin in self.skins_data
+            if not self.skins_data[skin]["isBase"]
+            and self.skins_data[skin]["tilePath"].split("/")[-2] == requested_champion_id
+        ]
+
+    def get_skins_data(self) -> dict:
+        return requests.get("https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/skins.json").json()
 
     def get_champions_data(self) -> dict:
-        url = f"https://ddragon.leagueoflegends.com/cdn/{self.latest_version}/data/pt_BR/champion.json"
-        response = requests.get(url)
-        return response.json()["data"]
+        return requests.get(f"https://ddragon.leagueoflegends.com/cdn/{self.latest_version}/data/pt_BR/champion.json").json()["data"]
 
     def get_versions(self) -> list:
-        response = requests.get(self.versions_url)
-        return response.json()
+        return requests.get("https://ddragon.leagueoflegends.com/api/versions.json").json()
 
 
 if __name__ == "__main__":
     riot = Riot()
-    champions_names = riot.get_champions_names()
+    database = riot.get_database()
 
     with open("database/response.json", "w", encoding="UTF-8") as f:
-        json.dump(champions_names, f, indent=4, ensure_ascii=False)
+        json.dump(database, f, indent=4, ensure_ascii=False)
